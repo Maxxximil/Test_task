0eaa913c61c5e124ec4d016bfd845670ecf7e59f
Maximil <borokm1@gmail.com>
2023-01-13 13:49:24 +0200

    Shield

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Controllers
+{
+    public class GameController : MonoBehaviour
+    {
+        public static GameController Instance;
+
+        public Transform SpawnPoint;
+        public Transform FinishPoint;
+        public GameObject PlayerPrefab;
+        public NavigationBacker navigationBacker;
+
+        public int Difficulty = 4;//Чем больше значение тем меньше зон
+        public float TimeToRespawn = 2f;
+        private float _currentTime;
+        private bool _isRespawned = false;
+
+        private void Awake()
+        {
+            Instance = this;
+
+        }
+        private void Start()
+        {
+            _currentTime = TimeToRespawn;
+        }
+
+        private void Update()
+        {
+            if (_isRespawned) return;
+            if (_currentTime > 0)
+            {
+                _currentTime -= Time.deltaTime;
+            }
+            else
+            {
+                SpawnPlayer();
+                UIController.Instance.Shield.SetActive(true);
+            }
+        }
+
+        public void SpawnPlayer()
+        {
+            Vector3 pos = SpawnPoint.position;
+            pos.y += 1f;
+            GameObject player = Instantiate(PlayerPrefab, pos, Quaternion.identity);
+            _isRespawned = true;
+            //Player.player.MoveToLocation(FinishPoint.position);
+            navigationBacker.BakeNavMesh();
+
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using Scripts.Controllers;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,29 +7,59 @@ using UnityEngine.AI;
 public class Player : MonoBehaviour
 {
     public static Player player;
-    
+
+    public Material activeShield;
+    public Material nonActiveShield;
+    public Object Explosion;
     
     public NavMeshAgent Agent;
-    
-    
+
+    public bool IsShieldActive = false;
+
     private void Awake()
     {
         player = this;    
     }
 
-    
-
-
-    private void Update()
+    private void Start()
     {
        Agent.SetDestination(GameController.Instance.FinishPoint.position);
     }
 
-    private void OnTriggerEnter(Collider other)
+
+
+    public void ActiveShield(bool isActive)
     {
-        if (other.CompareTag("Death"))
+        if (isActive)
         {
-            Debug.Log("Death");
+            gameObject.GetComponent<MeshRenderer>().material = activeShield;
+        }
+        else
+        {
+            gameObject.GetComponent<MeshRenderer>().material = nonActiveShield;
+
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Death") && !IsShieldActive) 
+        {
+            //StartCoroutine(GameOver());
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        Agent.isStopped = true;
+        GameObject go = (GameObject)Instantiate(Explosion);
+        go.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        //yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+        GameController.Instance.SpawnPlayer();
+    }
+
+    
 }
